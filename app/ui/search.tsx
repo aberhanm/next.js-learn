bd7660b42e790ffd4a70bdcd08146dcaee64ff33
John Doe <18487378910@163.com>
2025-01-03 09:33:31 +0800

    update1/3

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -8,7 +8,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const handleSearch = useDebouncedCallback((value: string) => {
     const params = new URLSearchParams(searchParams);
-
+    params.set("page", "1");
     if (value) {
       params.set("query", value);
     } else {
@@ -24,6 +24,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         Search
       </label>
       <input
+        id="search"
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
